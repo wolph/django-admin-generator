c8cd2f3e498191b8fa7b796dfa018ffe7262fa91
Rick van Hattem <Wolph@wol.ph>
2016-12-12 22:56:24 +0100

    fixed description for pypi

--- django_admin_generator/__about__.py
+++ django_admin_generator/__about__.py
@@ -2,7 +2,7 @@ __package_name__ = 'django-admin-generator'
 __version__ = '2.0.0'
 __author__ = 'Rick van Hattem'
 __author_email__ = 'Wolph@Wol.ph'
-__description__ = '''Django Admin Generator is a management command to
-automatically generate a Django `admin.py` file for given apps/models.'''
+__description__ = ('Django Admin Generator is a management command to '
+'automatically generate a Django `admin.py` file for given apps/models.')
 __url__ = 'https://github.com/WoLpH/django-admin-generator/'
 
