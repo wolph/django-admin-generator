4150110ee927d500b311db2d37d0a1b781eb66be
Rick van Hattem <Wolph@wol.ph>
2018-04-05 21:35:21 +0200

    updated to support django 1.11 and django 2.0

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import setuptools
+from setuptools.command.test import test as TestCommand
 
 # To prevent importing about and thereby breaking the coverage info we use this
 # exec hack
@@ -11,8 +13,21 @@ with open('django_admin_generator/__about__.py') as fp:
 if os.path.isfile('README.rst'):
     long_description = open('README.rst').read()
 else:
-    long_description = 'See http://pypi.python.org/pypi/%s/' % (
-        about['__package_name__'])
+    long_description = ('See http://pypi.python.org/pypi/' +
+                        about['__package_name__'])
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 
 if __name__ == '__main__':
@@ -25,13 +40,27 @@ if __name__ == '__main__':
         url=about['__url__'],
         license='BSD',
         packages=setuptools.find_packages(),
-        long_description=long_description,
         install_requires=[
-            'django-utils2>=2.2.1',
+            'django-utils2>=2.5.0',
             'six',
         ],
-        tests_require=['pytest'],
-        setup_requires=['pytest-runner'],
+        extras_require={
+            'docs': [
+                'django>=1.11',
+                'mock',
+                'sphinx>=1.6.0',
+            ],
+            'tests': [
+                'pytest',
+                'pytest-cache',
+                'pytest-cov',
+                'pytest-django',
+                'pytest-flakes',
+                'pytest-pep8',
+            ],
+        },
+        long_description=long_description,
+        cmdclass={'test': PyTest},
         classifiers=['License :: OSI Approved :: BSD License'],
     )
 
