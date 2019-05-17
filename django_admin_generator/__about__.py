a04e5d83ef937e651d3eb2af2f50c677697a60bd
Rick van Hattem <Wolph@wol.ph>
2019-05-17 15:37:43 +0200

    Incrementing version to v2.2.0

--- django_admin_generator/__about__.py
+++ django_admin_generator/__about__.py
@@ -1,5 +1,5 @@
 __package_name__ = 'django-admin-generator'
-__version__ = '2.1.0'
+__version__ = '2.2.0'
 __author__ = 'Rick van Hattem'
 __author_email__ = 'Wolph@Wol.ph'
 __description__ = ' '.join(('''
