bcf49eae99380ab963db03ea8422a59fb8097a3f
Rick van Hattem <Wolph@wol.ph>
2023-01-25 12:21:15 +0100

    almost working

--- setup.py
+++ setup.py
@@ -65,9 +65,11 @@ if __name__ == '__main__':
             'Intended Audience :: Developers',
             'License :: OSI Approved :: BSD License',
             'Natural Language :: English',
-            'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
         ],
     )
 
